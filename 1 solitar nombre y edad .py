0bb7fec320416ef4c22d86311ba212785d00eb30
Frailin22 <frailinteacuevas@gmail.com>
2025-02-13 21:28:40 -0400

    Add files via upload
    ejercicios básicos unad

---
+++ 1 solitar nombre y edad .py
@@ -0,0 +1,7 @@
+print("hola este programa te pide el nombre y edad y lo escribre en la pantalla")
+
+nombre = input("pon tu nombre: ")
+edad = input("pon tu edad: ")
+print("hola " + nombre +", tienes " + edad + "años")
+
+
